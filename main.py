6591549fef39f806a85979c21b9dd41b05a1d8d8
kasturiapte3011 <kasturiapte765@gmail.com>
2025-10-20 20:57:46 +0530

    v1

--- main.py
+++ main.py
@@ -1,22 +1,105 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from vector import retriever
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from pypdf import PdfReader
+import os
+import io
+from typing import Dict
 
+# -----------------Vector database-----------------------
+# embedding model -> all-minilm:l6-v2
+# vector db -> Chroma db
+embedder = OllamaEmbeddings(model="all-minilm:l6-v2")
+
+db_location = "database"
+db_flag = not os.path.exists(db_location)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  
+    chunk_overlap=200, 
+)
+        
+vector_store = Chroma(
+    collection_name="demo",
+    persist_directory=db_location,
+    embedding_function=embedder)
+
+
+retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 5})
+
+def extract_text_from_pdf(file_bytes:bytes) -> str:
+    """
+    Extracts text from a PDF given in form of bytes.
+    """
+    reader = PdfReader(io.BytesIO(file_bytes))
+    text = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
+    return text
+
+def embed(pdfs:Dict[str, bytes]) -> None:
+    """
+    Embeds multiple PDFs given as (filename, file_bytes).
+    """
+    text = []
+    for pdf in pdfs.items():
+        text.append(Document(
+            page_content=extract_text_from_pdf(pdf[1]),
+            metadata={"source": pdf[0]}))
+    docs = text_splitter.split_documents(text)
+    ids = [str(i) for i in range(len(docs))]
+    vector_store.add_documents(documents=docs, ids=ids)
+    return
+
+def clear_db() -> None:
+    """
+    Truncates the contents of vector database.
+    """
+    ids = vector_store._collection.get()["ids"]
+    if len(ids) >= 1:
+        vector_store.delete(ids=ids)
+    return
+# -----------------LLM call-----------------------
+# LLM -> LLaMa 3
+# Server -> Ollama
 model = OllamaLLM(model="llama3")
-template = """
-You are an agent thet helps user chat with their documents. You are provided with relevent chunks of text from the documents and you answer user query based on the given text chunks.
 
-relevent text chunks : {chunks} 
+template = """
+You are a RAG assistant. Your answers must be derived from the supplied context. Do not hallucinate or invent false facts. If the relavent information is missing or unclear in the context, respond only with: \"I don't know\". If neither history nor the relevant context is provided, respond only with \"No relevant context provided\".Provide responses that are accurate, explanatory and directly grounded in the context. Use chat history provided below for maintaining context.
+
+Chat History : {history}
+relevant text chunks : {chunks} 
 user query : {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
+
+conversation_store = Chroma(
+    collection_name="chat_memory",
+    embedding_function=embedder,
+    persist_directory="./conversation_history")
+
+memory = VectorStoreRetrieverMemory(
+    retriever=conversation_store.as_retriever(), 
+    memory_key="history", 
+    input_key="question")
+
 chain = prompt | model
 
-while True:
-    query = input("Ask your question (enter q to quit):")
-    if query == 'q':
-        break
-    
+def result(query:str, new_chat:bool) -> str:
+    if new_chat == True:
+        conversation_store.delete(ids=conversation_store._collection.get()["ids"])
+
     chunks = retriever.invoke(query)
-    result = chain.invoke({"chunks":chunks, "question":query})
-    print(result) 
+    result = chain.invoke({
+        "history":memory.load_memory_variables({"question": query})["history"],
+        "chunks":chunks, 
+        "question":query
+        })
+    memory.save_context({"input": query}, {"output": result})
+    return result
---
+++ ui.py
@@ -0,0 +1,118 @@
+import streamlit as st
+from main import result, embed, clear_db
+
+# Streamlit page setup
+st.set_page_config(page_title="PapyrusAI", layout="wide")
+
+st.markdown(
+"""
+<style>
+    .st-key-chat {
+        padding-bottom: 10px;
+    }
+
+    .st-key-new {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    @media (max-width: 1200px) {
+        .st-key-new {
+            transform: translate(65%, 35%);
+        }
+    }
+
+    @media (max-width: 992px) {
+        .st-key-new {
+            transform: translate(65%, 35%);
+        }
+    }
+
+    @media (max-width: 768px) {
+        .st-key-new {
+            transform: translate(65%, 35%);
+        }
+    }
+
+    @media (max-width: 480px) {
+        .st-key-new {
+            transform: translate(65%, 35%);
+        }
+    }
+
+</style>
+""",
+unsafe_allow_html=True
+)
+
+
+# -------------------- Session State --------------------
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "uploaded_files" not in st.session_state:
+    st.session_state.uploaded_files = {}
+if "user_query" not in st.session_state:
+    st.session_state.user_query = ""
+if "new_chat" not in st.session_state:
+    st.session_state.new_chat = True
+# -------------------- Header --------------------
+left, right = st.columns([0.50, 0.50])
+with left:
+    st.title("PapyrusAI🍀")
+    st.caption("Add your PDFs and chat with them with the power of LLaMa3")
+with right:
+    if st.button("🆕 New Chat", help="Start new chat", key="new"):
+        st.session_state.messages = []
+        st.session_state.new_chat = True
+# -------------------- Right Panel (Tools) --------------------
+
+with st.sidebar:
+    st.title("⚙️ Upload your documents")
+    with st.container(border=True):
+        st.markdown("**Add Files**")
+        files = st.file_uploader(
+            "Upload PDFs",
+            type=["pdf"],
+            accept_multiple_files=True,
+            help="You can upload multiple PDF files.")
+        if files:
+            for f in files:
+                st.session_state.uploaded_files[f.name] = f.getvalue()
+
+        if len(st.session_state.uploaded_files)>=1:
+            if st.button("📥 Embed", type="primary", use_container_width=True):
+                selected_files = {file: st.session_state.uploaded_files[file] for file in st.session_state.uploaded_files.keys()}
+                embed(selected_files)
+                st.session_state.uploaded_files = {}
+                st.success("Embedding complete!")
+        else:
+            st.info("No files uploaded yet.")
+
+    with st.container(border=True):
+        st.markdown("**Embedded Docs**")
+        st.caption("(List embedded docs here)")
+        cols = st.columns(2)
+        with cols[0]:
+            if st.button("🔄 Refresh", use_container_width=True):
+                st.rerun()
+        with cols[1]:
+            if st.button("🧹 Clear DB", use_container_width=True):
+                clear_db()
+# -------------------- Chat (Main) --------------------
+user_query = st.chat_input("Ask about your PDFs...", key="input")
+with st.container(key="chat"):
+    st.subheader("💬 Chat")        
+    if user_query:
+        st.session_state.messages.append({"role": "user", "content": user_query})
+        with st.spinner("Thinking..."):
+            try:
+                response = result(user_query, st.session_state.new_chat)
+                if st.session_state.new_chat:
+                    st.session_state.new_chat = False
+            except Exception as e:
+                response = f"Error calling result(): {e}"
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+    for msg in st.session_state.messages:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
